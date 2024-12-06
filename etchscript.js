c2e50ce9cc0a5bb96fe3025c81638164135d7334
swl23 <139284802+swl23@users.noreply.github.com>
2024-12-06 13:42:18 -0800

    Create reset button with window prompt to get user input for grid size

--- etchscript.js
+++ etchscript.js
@@ -1,4 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
+    const resetButton = document.createElement('button');
+    resetButton.setAttribute('id', 'reset-button');
+    resetButton.textContent = 'Reset';
+    resetButton.addEventListener('click', () => {
+        const number = window.prompt('Please enter a number (100 or smaller) to generate the new grid.');
+        generateGrid(number);
+    });
+    body.prepend(resetButton);
+
     for (i = 0; i < 16; i++) {
         createRow(i)
     };
